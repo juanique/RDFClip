8cb5f83ca9ebb0d2cf7367f0088dafa6905cdd6d
Juan Enrique Munoz <juanique@gmail.com>
2011-09-15 13:50:22 -0700

    working version with django templates

--- base/templatetags/resource.py
+++ base/templatetags/resource.py
@@ -1,28 +1,58 @@
 from django import template
+from django.template import Template, Context
 import settings
 
 register = template.Library()
 
 class ResourceNode(template.Node):
-    def __init__(self,row, res, label):
+    def __init__(self,row, res, label, property_column = False):
         self.row_variable = template.Variable(row)
         self.resource_column = res
         self.label_column = label
+        self.property_column = property_column
 
     def render(self, context):
         row = self.row_variable.resolve(context)
         resource = row[self.resource_column]
-        label = row.get(self.label_column, resource)
+    
+        local_context = {}
+        local_context['resource'] = resource.value
+        local_context['label'] = row.get(self.label_column, resource).value
+        if self.property_column:
+            local_context['property'] = row[self.property_column].value
 
-        if resource.type =='uri':
-            return "<A title='%s' href='%s'>%s</A>" % (resource.value, resource.value, label.value)
-        else:
-            return "<SPAN>%s</SPAN>" % (resource.value)
+        anchor = """ <A title='{{resource}}' 
+                        href='{{resource}}' 
+                        {% if property %}
+                            property='{{property}}'
+                        {% endif %}
+                     >
+                        
+                        {{label}}</A>
+                """
+
+        span = """<SPAN
+                    {% if property %}
+                        property='{{property}}'
+                    {% endif %}
+        
+                >{{resource}}</SPAN>
+               """
+
+        template = Template(anchor if resource.type =='uri' else span)
+        template = template.render(Context(local_context))
+
+        return Template(template).render(context)
+
 
 def resource_tag(parser, token):
     tokens = token.split_contents()
     (tag, row, uri, label) = tuple(tokens[:4])
 
-    return ResourceNode(row, uri, label)
+    prop = False
+    if len(tokens) > 4:
+        prop = tokens[4]
+
+    return ResourceNode(row, uri, label, prop)
 
 register.tag('resource',resource_tag)
